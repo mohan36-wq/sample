569fba48d5335a24a108f30195dbd3fa7a2ae0e9
areymomo <69445481+areymomo@users.noreply.github.com>
2020-08-10 09:32:35 +0530

    random line

--- samples.java
+++ samples.java
@@ -2,5 +2,6 @@ class samples{
     public static void main(String[] args){
         System.out.println("Hello Samples");
         System.out.println("Welcome message");
+        random line
     }
 }
